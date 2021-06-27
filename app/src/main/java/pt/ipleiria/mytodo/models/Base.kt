60ca864d68c66966e5dc200dad3bc91d50232986
David Cervantes Pérez <dacert@gmail.com>
2021-06-27 20:48:16 +0100

    add timestamp to dtos

--- app/src/main/java/pt/ipleiria/mytodo/models/Base.kt
+++ app/src/main/java/pt/ipleiria/mytodo/models/Base.kt
@@ -1,5 +1,6 @@
 package pt.ipleiria.mytodo.models
 
 import java.io.Serializable
+import java.util.*
 
-interface Base: Serializable { val id: String }
+interface Base: Serializable { val id: String; val timestamp: Date? }
